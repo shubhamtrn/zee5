54a31e8bf242c9980defa405d6507e192d9ebbd1
shubhamtrn <111489240+shubhamtrn@users.noreply.github.com>
2022-08-18 14:14:19 +0530

    java new file

---
+++ new_file.java
@@ -0,0 +1,12 @@
+public class company{
+  String name;
+  int id;
+  int emp_quant;
+  
+  company(String name, int id , int emp_quant){
+    this.name=name;
+    this.id=id;
+    this emp_quant=quant;
+  void display(){
+    System.out.println("name:"+name+"id:"+id);
+  }
